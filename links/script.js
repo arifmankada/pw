1eb61bd231ddcf940a497f2a19222409a50fd489
Arif Mankada <64087217+arifmankada@users.noreply.github.com>
2023-10-18 17:43:55 +0530

    Update script.js

--- links/script.js
+++ links/script.js
@@ -4,6 +4,7 @@ const apiKey = 'AIzaSyBrPxK2PojYKuYZ0bl1IXQnqc39PSSQMV8'; // Replace with your Y
 
 // Sample data - replace with your own video IDs
 const videoIds = [
+    "Ua_l3gDmf2c"
     "p_bml_rHnbo",
     "eFJGyT3C-Y0",
     "an7ajeQb76s",
