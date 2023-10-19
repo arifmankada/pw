8af7df3c4fdea3595ef716c997c0119a3e9a3d3d
Arif Mankada <64087217+arifmankada@users.noreply.github.com>
2023-10-19 17:23:13 +0530

    Update script.js

--- links/script.js
+++ links/script.js
@@ -4,12 +4,12 @@ const errorDisplay = document.getElementById("errorDisplay");
 const apiKey = 'AIzaSyBrPxK2PojYKuYZ0bl1IXQnqc39PSSQMV8'; // Replace with your YouTube Data API key
 const videoIds = [
     "5uLnYmM-NaI",
-    "92r4X9kYfzA&t",
-    "WgVH2q300Hw&t",
-    "CBLrlAD9uA8&t",
-    "NGY1ch9hA98&t",
+    "92r4X9kYfzA",
+    "WgVH2q300Hw",
+    "CBLrlAD9uA8",
+    "NGY1ch9hA98",
     "J8n_eTGvUvU",
-    "kOUoQSnKwSw&t",
+    "kOUoQSnKwSw",
     "dG2q9yhjpGc",
     "4-l7cemjwyI",
     "Ua_l3gDmf2c",
@@ -20,15 +20,15 @@ const videoIds = [
 ];
 
 async function populateVideoList() {
-    videoList.innerHTML = ""; // Clear the existing video list
-    errorDisplay.textContent = ""; // Clear any previous error messages
+    videoList.innerHTML = "";
+    errorDisplay.textContent = "";
 
     for (const videoId of videoIds) {
         try {
             const response = await fetch(`https://www.googleapis.com/youtube/v3/videos?key=${apiKey}&part=snippet&id=${videoId}`);
             const data = await response.json();
-            const video = data.items[0];
-            if (video) {
+            if (data.items && data.items.length > 0) {
+                const video = data.items[0];
                 const li = document.createElement("li");
                 li.innerHTML = `
                     <div class="video-thumbnail">
@@ -39,12 +39,11 @@ async function populateVideoList() {
                 `;
                 videoList.appendChild(li);
             } else {
-                // Handle the case where video data is not found
-                console.error(`Video data not found for ${videoId}`);
+                errorDisplay.textContent = `No video data found for ${videoId}`;
             }
         } catch (error) {
             console.error(`Error fetching video data for ${videoId}:`, error);
-            errorDisplay.textContent = "Error: Unable to fetch video data. Please try again later.";
+            errorDisplay.textContent = `Error: Unable to fetch video data for ${videoId}`;
         }
     }
 }
