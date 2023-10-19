c602c5181425ca68a0624214901a1a57bc1331f3
Arif Mankada <64087217+arifmankada@users.noreply.github.com>
2023-10-19 17:26:24 +0530

    Update script.js

--- links/script.js
+++ links/script.js
@@ -1,6 +1,5 @@
 const videoList = document.getElementById("videoList");
 const filterInput = document.getElementById("filterInput");
-const errorDisplay = document.getElementById("errorDisplay");
 const apiKey = 'AIzaSyBrPxK2PojYKuYZ0bl1IXQnqc39PSSQMV8'; // Replace with your YouTube Data API key
 const videoIds = [
     "5uLnYmM-NaI",
@@ -15,35 +14,43 @@ const videoIds = [
     "Ua_l3gDmf2c",
     "p_bml_rHnbo",
     "eFJGyT3C-Y0",
-    "an7ajeQb76s",
+    "an7ajeQb76s"
     // Add more video IDs here
 ];
 
+async function fetchVideoData(videoId) {
+    try {
+        const response = await fetch(`https://www.googleapis.com/youtube/v3/videos?key=${apiKey}&part=snippet&id=${videoId}`);
+        if (!response.ok) {
+            throw new Error(`Failed to fetch: ${response.status} ${response.statusText}`);
+        }
+        const data = await response.json();
+        if (data.items && data.items.length > 0) {
+            return data.items[0];
+        } else {
+            throw new Error(`No video data found for ${videoId}`);
+        }
+    } catch (error) {
+        console.error(`Error fetching video data for ${videoId}:`, error);
+        return null;
+    }
+}
+
 async function populateVideoList() {
     videoList.innerHTML = "";
-    errorDisplay.textContent = "";
 
     for (const videoId of videoIds) {
-        try {
-            const response = await fetch(`https://www.googleapis.com/youtube/v3/videos?key=${apiKey}&part=snippet&id=${videoId}`);
-            const data = await response.json();
-            if (data.items && data.items.length > 0) {
-                const video = data.items[0];
-                const li = document.createElement("li");
-                li.innerHTML = `
-                    <div class="video-thumbnail">
-                        <a href="https://www.youtube.com/watch?v=${videoId}" target="_blank">
-                            <img src="${video.snippet.thumbnails.medium.url}" alt="Video Thumbnail">
-                        </a>
-                    </div>
-                `;
-                videoList.appendChild(li);
-            } else {
-                errorDisplay.textContent = `No video data found for ${videoId}`;
-            }
-        } catch (error) {
-            console.error(`Error fetching video data for ${videoId}:`, error);
-            errorDisplay.textContent = `Error: Unable to fetch video data for ${videoId}`;
+        const video = await fetchVideoData(videoId);
+        if (video) {
+            const li = document.createElement("li");
+            li.innerHTML = `
+                <div class="video-thumbnail">
+                    <a href="https://www.youtube.com/watch?v=${videoId}" target="_blank">
+                        <img src="${video.snippet.thumbnails.medium.url}" alt="Video Thumbnail">
+                    </a>
+                </div>
+            `;
+            videoList.appendChild(li);
         }
     }
 }
